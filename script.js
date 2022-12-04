41d5ac2f6445753498ccf0ab08eb1ef13623b9ec
roberttsouza <roberttsouzzaa@gmail.com>
2022-12-04 17:28:44 -0300

    :construction: formatando as regras que serão aceitas do campos de input

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 const toggleSidebar = () => document.body.classList.toggle("open");
 
+const work = document.getElementById("work").value;
+const breakTime = document.getElementById("break").value;
+const rounds = document.getElementById("rounds").value;
+
+const regex = /^([0-9]){1,3}$/g;
+
+const foundWork = (work,breakTime,rounds).match(regex);
+
+if (work && breakTime && rounds == foundWork){
+  console.log("Tudo certo")
+}else{
+  console.log("Algo deu errodo")
+}
+
+
+console.log(work, breakTime, rounds)
+
+setTimeout(function reload(){
+location.reload(); 
+},1000) 
