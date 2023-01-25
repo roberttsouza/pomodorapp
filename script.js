acfb324461051b0e231f2768135e8ab17dc079ac
roberttsouza <roberttsouzzaa@gmail.com>
2023-01-25 19:08:29 -0300

    :construction: feito a função de correr e pausar entre as sessions

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const toggleSidebar = () => document.body.classList.toggle("open");
 
-const work = document.getElementById("work").value;
-const breakTime = document.getElementById("break").value;
-const rounds = document.getElementById("rounds").value;
-const som = document.querySelector("#som")
+let work = document.getElementById("work").value;
+let breakTime = document.getElementById("break").value;
+let rounds = document.getElementById("rounds").value;
+let som = document.querySelector("#som");
+let currentRound = 1;
+const sessionNow = document.querySelector(".now-session").textContent;
+const nextSession = document.querySelector(".rem-sessions").textContent;
 
 const regex = /^([0-9]){1,3}$/g;
 
@@ -21,7 +24,7 @@ if (work && breakTime && rounds == foundWork){
 
 // declarando as var do time
 let time = document.querySelector('#work').value * 60;
-let intervalId; 
+let intervalId;
 
 //atrubindo uma regra para que se o minuto ou segundo for menor que 10, atribuir um zero na frente para sempre ficar com dois numeros
 function twoDigits(digits){
@@ -38,9 +41,21 @@ start = () =>{
     time--;
     updateTimer();
     if (time === 0){
+      if (currentRound === rounds) {
+        // tudo finalizado, não faz nada
+    } else {
+        if (currentRound % 2 !== 0) {
+            // inicia o tempo de descanso
+            time = breakTime * 60;
+            currentRound++;
+        } else {
+            // inicia o tempo de trabalho
+            time = work * 60;
+            currentRound++;
+        }
+    }
       clearInterval(intervalId);
       som.play()
-      
     }
   }, 1000);
 }
@@ -55,13 +70,14 @@ reset = () => {
 }
 
 //atribuindo um evendo para que quando o valor input por alterado, ele muda automaticamente
-const input = document.querySelector('#work');
-input.addEventListener("input", function(){
+const inputWork = document.querySelector('#work');
+inputWork.addEventListener("input", function(){
   time = parseInt(this.value * 60); //para converter o valor do input para inteiro e atribuindo esse valor para a variável global e para o time começar a contar apartir do valor informado no input 
   min= document.querySelector("#work").value
   sec=0
   document.querySelector('.watch').innerHTML = `${twoDigits(min)}:${twoDigits(sec)}`
 })
+
 
 //função responsável por atualizar o time em segundos e converter para numeros inteiros
 function updateTimer(){
